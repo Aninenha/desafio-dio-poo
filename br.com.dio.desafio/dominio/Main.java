fe84740e6c2b48240eeb5b4cffc90af10433e9d6
GoldenGrace <anaflaviafiorini@gmail.com>
2024-06-15 18:15:05 -0300

    concluida uma aula do desafio

--- br.com.dio.desafio/dominio/Main.java
+++ br.com.dio.desafio/dominio/Main.java
@@ -1,7 +1,36 @@
 package br.com.dio.desafio;
 
+import br.com.dio.desafio.dominio.Curso;
+
+import java.time.LocalDate;
+
+/**
+ * The type Main.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Curso curso1 = new Curso();
+
+        curso1.setCargaHoraria(8);
+        curso1.setDescricao("descrição do curso");
+        curso1.setTitulo("curso java");
+
+        Curso curso2 = new Curso();
+
+        curso2.setCargaHoraria(9);
+        curso2.setDescricao("descrição do curso");
+        curso2.setTitulo("curso kotlin");
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição da mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
     }
 }
--- br.com.dio.desafio/dominio/Mentoria.java
+++ br.com.dio.desafio/dominio/Mentoria.java
@@ -1,7 +1,54 @@
-package br.com.dio.desafio.dominio;
+package br.com.dio.desafio;
 
+import java.time.LocalDate;
+
+/**
+ * The type Mentoria.
+ */
 public class Mentoria {
-    String titulo;
-    String descricao;
-    LocalDate data;
+	/**
+	 * The Titulo.
+	 */
+	String titulo;
+	/**
+	 * The Descricao.
+	 */
+	String descricao;
+	/**
+	 * The Data.
+	 */
+	LocalDate data;
+
+	@Override
+	public String toString() {
+		return "Mentoria{" +
+				"titulo='" + titulo + '\'' +
+				", descricao='" + descricao + '\'' +
+				", data=" + data +
+				'}';
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
 }
